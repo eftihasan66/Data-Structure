875fde54d9144d02925286da9e59afbd19f6037f
Efti Hasan <92861990+eftihasan66@users.noreply.github.com>
2024-11-16 23:44:28 +0600

    Add files via upload

---
+++ Binary_Tree/Operation_on_Binary_tree/count_number_of_nodes.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public :
+        int val;
+        Node* left;
+        Node* right;
+    Node(int val)
+    {
+        this->val = val ;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* input()
+{
+    int val; cin>>val ;
+    Node* root;
+   if(val==-1) root=NULL;
+   else root = new Node(val);
+
+    queue<Node*> q;
+    if(root) q.push(root);
+
+    while(!q.empty())
+    {
+        Node* f = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj
+        int l,r; cin>>l>>r;
+        Node* mYleft;
+        Node* mYright;
+
+        if(l==-1) mYleft=NULL;
+        else mYleft = new Node(l);
+
+        if(r==-1) mYright=NULL;
+        else mYright = new Node(r);
+
+        f->left = mYleft;
+        f->right = mYright;
+        
+        // 3. children k push korsi
+       if(f->left) q.push(f->left);
+       if(f->right) q.push(f->right);
+    }
+    return root;
+}
+
+void level_order(Node* root)
+{
+    if(root==NULL) return;
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        //1. ber kore ana
+       Node* f = q.front();
+       q.pop();
+
+       //2. jaobtiyo ja kaaj
+       cout <<f->val <<" ";
+
+       //3. children rakha
+       if(f->left) q.push(f->left);
+       if(f->right) q.push(f->right);
+
+    } 
+}
+int count(Node* root)
+{
+    if(root==NULL) return 0;
+    
+    int l = count(root->left);
+    int r = count(root->right);
+    return l+r+1;
+}
+int main() 
+{
+    Node* root = input();
+    // level_order(root);
+    cout <<count(root);
+
+   return 0;
+}
---
+++ Binary_Tree/STL_PriorityQueue_Map_Set/stl_map.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+   map<string,int> mp;
+   mp["Sakib"]=75;
+   mp["Efti"]=15;
+   mp["Hasan"]=115;
+   mp["Mohammad"]=100;
+
+   cout <<mp["Efti"] ;
+
+   return 0;
+}
